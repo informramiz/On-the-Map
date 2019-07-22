71b7fc4208ae4224f7e83096f802ec243b01df54
Ramiz Raja <informramiz@gmail.com>
2019-07-22 14:47:42 +0500

    feat: Create project

---
+++ OntheMap/OntheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OntheMap
+//
+//  Created by Apple on 22/07/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
