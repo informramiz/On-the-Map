0f18cdfbc1d1e445d9d8e3bd826370425c01b536
Ramiz Raja <informramiz@gmail.com>
2019-07-22 16:31:34 +0500

    feat: Connect login view outlets and actions

--- OntheMap/OntheMap/Controller/ViewController.swift
+++ OntheMap/OntheMap/Controller/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        
+    }
 }
 
