46319941e0a6b300ca6c8c83fe9e65358f81db4b
Ramiz Raja <informramiz@gmail.com>
2019-07-22 16:00:14 +0500

    feat: Write utility extensions

---
+++ OntheMap/OntheMap/Controller/UIViewController+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController+Extension.swift
+//  OntheMap
+//
+//  Created by Apple on 22/07/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIViewController {
+    func openUrl(url: URL) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    func showErrorAlert(message: String) {
+        let alertController = UIAlertController()
+        alertController.message = message
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
