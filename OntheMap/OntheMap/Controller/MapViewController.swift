bd0da69e809ecff2a2e766b9b0f282af6695baaa
Ramiz Raja <informramiz@gmail.com>
2019-08-02 13:04:47 +0500

    feat: Provide pin view and handle pin taps

--- OntheMap/OntheMap/Controller/MapViewController.swift
+++ OntheMap/OntheMap/Controller/MapViewController.swift
@@ -45,5 +45,27 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController : MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reusePin = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reusePin) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reusePin)
+            pinView!.canShowCallout = true
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
     
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let mediaUrl = view.annotation?.subtitle! {
+                if let validUrl = URL(string: mediaUrl) {
+                    openUrl(url: validUrl)
+                }
+            }
+        }
+    }
 }
