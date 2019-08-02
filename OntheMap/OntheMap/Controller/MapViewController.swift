35fb4b5c4e5057f07cbe61ba72d7097341ddc113
Ramiz Raja <informramiz@gmail.com>
2019-08-02 12:13:59 +0500

    feat: Add map view controller

---
+++ OntheMap/OntheMap/Controller/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  OntheMap
+//
+//  Created by Ramiz Raja on 02/08/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MapViewController : MKMapViewDelegate {
+    
+}
