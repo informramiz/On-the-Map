3a26e845e6897b8685b36c173c6e38583a9c982f
Ramiz Raja <informramiz@gmail.com>
2019-08-12 16:46:52 +0500

    feat: Write utility extension

---
+++ OntheMap/OntheMap/extensions/String+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extension.swift
+//  OntheMap
+//
+//  Created by Ramiz Raja on 12/08/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    func isValidUrl() -> Bool {
+        guard self.isEmpty else { return false }
+        guard let url = URL(string: self) else { return false }
+        
+        return UIViewController.canOpenUrl(url: url)
+    }
+}
