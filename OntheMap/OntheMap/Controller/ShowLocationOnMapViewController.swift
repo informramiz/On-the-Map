83255440809b3ab6896664f89d3eb34aff886576
Ramiz Raja <informramiz@gmail.com>
2019-08-12 18:49:02 +0500

    feat: Add ViewController for show location UI

---
+++ OntheMap/OntheMap/Controller/ShowLocationOnMapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowLocationOnMapViewController.swift
+//  OntheMap
+//
+//  Created by Ramiz Raja on 12/08/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import UIKit
+
+class ShowLocationOnMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
